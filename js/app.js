abf4b06628d275c3183ed28eb48c6347fa121c73
Kevin <kevin@pixelkojan.se>
2016-07-09 17:58:21 +0200

    add createToDoItem function to handle creating and appending in a function

--- js/app.js
+++ js/app.js
@@ -1,34 +1,56 @@
 // TODO: Add IIFE, but leave for console testing in development
 'use strict';
 console.log('Start... Todo List program');
-
 // Get mockup elements from the HTML
 
 // Rewrote element selection to get by form element id and use the .elements property with the name selector to select the input elements name value
 var taskInput = document.getElementById('submitForm').elements['task-input'];
-
 // Get Submit button = #submitBtn
 var submitButton = document.getElementById('submitBtn');
 // Get Complete checkbox
 var checkbox = document.querySelector('input[type="checkbox"]');
-
 // Get Edit button
 var editButton = document.querySelector('.edit');
 // Get Save button (Accessing this with foundations .success class that i will use for my Save Buttons)
 var saveButton = document.querySelector('.success');
 // Get delete button (Using Foundations .delete class for my Delete Button)
 var deleteButton = document.querySelector('.delete')
-
 // Get Incomplete List Items Holder Holder #incompletedList
 var incompletedListsHolder = document.getElementById('incompletedList');
-
 // Get Completed List Items Holder #completedList
 var completedListsHolder = document.getElementById('completedList');
 
 
+// Create Elements
+var createTodoItem = function () {
+
+
+    var todoItem = document.createElement('li');
+    var input = document.createElement('input');
+    var label = document.createElement('label');
+    var editBtn = document.createElement('button');
+    var deleteBtn = document.createElement('button');
+
+    input.type = 'checkbox';
+    label.innerText = taskInput.value;
+    editBtn.classList.add('edit', 'button');
+    editBtn.innerText = 'Edit';
+    deleteBtn.classList.add('alert', 'button');
+    deleteBtn.innerText = 'Delete';
+
+    todoItem.appendChild(input);
+    todoItem.appendChild(label);
+    todoItem.appendChild(editBtn);
+    todoItem.appendChild(deleteBtn);
+
+    return todoItem;
+};
+
 // Create a function to handle Add Tasks
 var addTask = function () {
     console.log('addTask function active...');
+    var todoItem = createTodoItem();
+    incompletedListsHolder.appendChild(todoItem);
     // 1. Add
     //     * User writes task 'go shopping'.
     //     * User push add new task button
